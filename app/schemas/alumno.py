0430216ebfad050fcbc53a6b2a5732045a2e72a1
Warita <warayasyp@gmail.com>
2025-12-26 18:53:55 +0100

    FEAT: Crer clase alumno

---
+++ app/schemas/alumno.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class LoginRequest(BaseModel):
+    usuario: str
+    contrasenya: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class AlumnoResponse(BaseModel):
+    usuario: str
+    nombre: str
+    idioma: Optional[str] = None
+
+    class Config:
+        from_attributes = True
